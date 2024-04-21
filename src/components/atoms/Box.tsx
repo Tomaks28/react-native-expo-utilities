49ef327bf7c80b633ed4c223cd365e0455cd5d68
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2024-04-21 12:49:01 +0200

    feat: add box atom component

---
+++ src/components/atoms/Box.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren, useMemo } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+interface BoxProps extends PropsWithChildren {
+  flex?: boolean;
+  horizontal?: boolean;
+  gap?: number;
+  center?: boolean;
+  xcenter?: boolean;
+  ycenter?: boolean;
+}
+
+export default function Box({ children, ...props }: BoxProps) {
+  const memoizedStyles = useMemo(
+    () => [
+      props.flex && styles.flex,
+      props.horizontal && styles.horizontal,
+      props.center && styles.center,
+      props.xcenter && styles.xcenter,
+      props.ycenter && styles.ycenter,
+      { gap: props.gap },
+    ],
+    [props],
+  );
+
+  return <View style={memoizedStyles}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  flex: { flex: 1 },
+  horizontal: { flexDirection: 'row' },
+  center: { justifyContent: 'center', alignItems: 'center' },
+  ycenter: { justifyContent: 'center' },
+  xcenter: { alignItems: 'center' },
+});
